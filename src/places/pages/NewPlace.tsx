daa27b8c90b18203f4be0befd99de1e32f0a15eb
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-27 13:10:08 +0530

    uploading images for new places

--- src/places/pages/NewPlace.tsx
+++ src/places/pages/NewPlace.tsx
@@ -11,6 +11,7 @@ import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
+import ImageUpload from "./../../shared/components/FormElements/ImageUpload";
 import { useForm } from "./../../shared/hooks/form-hook";
 import "./PlaceForm.css";
 
@@ -34,6 +35,10 @@ const NewPlace = () => {
         value: "",
         isValid: false,
       },
+      image: {
+        value: null,
+        isValid: false,
+      },
     },
     false
   );
@@ -41,17 +46,15 @@ const NewPlace = () => {
   const placeSubmitHandler = async (event: any) => {
     event.preventDefault();
     try {
-      await sendRequest(
-        PlaceAPI.newPlace,
-        "POST",
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
-          address: formState.inputs.address.value,
-          creator: auth.userId,
-        }),
-        { "Content-Type": "application/json" }
-      );
+      const formData = new FormData();
+
+      formData.append("title", formState.inputs.title.value);
+      formData.append("description", formState.inputs.description.value);
+      formData.append("address", formState.inputs.address.value);
+      formData.append("creator", auth.userId as string);
+      formData.append("image", formState.inputs.image.value);
+
+      await sendRequest(PlaceAPI.newPlace, "POST", formData);
 
       history.push("/");
     } catch (err) {}
@@ -87,6 +90,11 @@ const NewPlace = () => {
           errorText="Please enter a valid address"
           validators={[VALIDATOR_REQUIRE()]}
           onInput={inputHandler}
+        />
+        <ImageUpload
+          id="image"
+          onInput={inputHandler}
+          errorText="Please provide an image"
         />
         <Button type="submit" disabled={!formState.isValid}>
           ADD PLACE
