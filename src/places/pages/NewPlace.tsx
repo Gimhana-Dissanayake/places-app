30cb9d9785ea08bb00be8c7df2590e95b7e5bb69
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-28 07:20:41 +0530

    end of section 12

--- src/places/pages/NewPlace.tsx
+++ src/places/pages/NewPlace.tsx
@@ -51,7 +51,7 @@ const NewPlace = () => {
       formData.append("title", formState.inputs.title.value);
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
-      formData.append("creator", auth.userId as string);
+
       formData.append("image", formState.inputs.image.value);
 
       await sendRequest(PlaceAPI.newPlace, "POST", formData, {
